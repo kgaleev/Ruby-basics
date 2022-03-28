06263fd6092142dc7feb02319c83c4c533ba7472
kgaleev <kkgaleev@gmail.com>
2022-03-29 03:38:05 +0500

    changed all names to english, changed names to more intuitive for readability, squeezed code, rewrote Train class

--- lesson 3/poezda.rb
+++ lesson 3/poezda.rb
@@ -2,116 +2,116 @@ class Station # Класс Station (Станция)
   def initialize(name)
     # Имеет название, которое указывается при ее создании
     @name = name
-    @station = [] # все поезда на станции
-    @tipg = [] # грузовые поезда
-    @tipp = [] # пассажирские поезда
+    @trains = [] # все поезда на станции
+    #@trains_type_cargo = [] # грузовые поезда;
+    #@trains_type_passenger = [] # пассажирские поезда
   end
 
   # Может принимать поезда (по одному за раз)
   def arrival(train)
     #@station.insert(0, train)
-    @station.push(train) #poprobovat @station << train
-    if train.tip == 1
-      @tipg.push(train.tip)
-    elsif train.tip == 2
-      @tipp.push(train.tip)
-    end
+    @trains.push(train) #poprobovat @trains << train
+    #if train.type == :cargo
+    #  @trains_type_cargo.push(train.type)
+    #elsif train.type == :passenger
+    #  @trains_type_passenger.push(train.type)
+    #end
   end
 
-  attr_reader :name # ридер, чтобы можно было в машрут забирать только названия станций
+  # ридер :name, чтобы можно было в машрут забирать только названия станций
+  attr_reader :name, :trains # Может возвращать список всех поездов на станции, находящихся в текущий момент
 
-  # Может возвращать список всех поездов на станции, находящихся в текущий момент
-  def spisok_all
-    @station
-  end
+  #def list_trains
+  #  @trains
+  #end
 
   # Может возвращать список поездов на станции по типу кол-во грузовых, пассажирских
-  def spisok_tip
-    @tipg.each { |tipg| puts tipg }
-    @tipp.each { |tipp| puts tipp } # ruby всегда чё-то возвращает, а each - массив
+  #def trains_by_type
+  def trains_by_type(type)
+    trains.select {|train| train.type == type}.count # без .count - по типу, а с .count - сосчитает
+
+    #@trains_type_cargo.each { |cargo| puts cargo }
+    #@trains_type_passenger.each { |passenger| puts passenger } # ruby всегда чё-то возвращает, а each - массив
     #[tipg, tipp].flatten.each
     #nil - можно закрывать, чтобы метод не возвращал кучу данных в ответе
     #puts Train::TYPE
-    tipi = [@tipg, @tipp].flatten
-    #@tipg.tally
-    #@tipp.tally
-    tipi.tally
+    #[@trains_type_cargo, @trains_type_passenger].flatten.tally
+
   end
 
   # Может отправлять поезда (по одному за раз, при этом, поезд удаляется из списка поездов, находящихся на станции).
-  def otpravit(train)
-    @station.delete(train)
-    if train.tip == 1
-      @tipg.delete_at(-1)
-    elsif train.tip == 2
-      @tipp.delete_at(-1)
-    end
+  def dispatch(train)
+    @trains.delete(train)
+    #if train.type == :cargo
+    #  @trains_type_cargo.delete_at(-1)
+    #elsif train.type == :passenger
+    #  @trains_type_passenger.delete_at(-1)
+    #end
   end
 
 end
 
 class Route # Класс Route (Маршрут) Имеет начальную и конечную станцию, а также список промежуточных станций
 
-  # хорошо бы куда-то воткнуть массив всех станций
-
-  def initialize(start, finish)
+  def initialize(first_station, last_station)
     #Начальная и конечная станции указываютсся при создании маршрута
-    @route = [nil, start, finish]
+    @stations = [first_station, last_station]
+    # @last_station = last_station # для проверки при движении поезда до конечной
   end
 
-  attr_reader :route
+  attr_reader :stations
+  #attr_reader :last_station
 
   def add_station(station)
     #Может добавлять промежуточную станцию в список
-    @route.insert(-2, station) #промежуточные могут добавляться между ними
+    @stations.insert(-2, station) #промежуточные могут добавляться между ними
   end
 
   def remove_station(station)
     #Может удалять промежуточную станцию из списка
-    @route.delete(station)
+    @stations.delete(station)
   end
 
   #Может выводить список всех станций по-порядку от начальной до конечной
-  def list_station
-    puts @route # peredelat na ['Алексей', 'Евгений'].each { |name| puts name }
+  def list_stations
+    puts @stations # peredelat na ['Алексей', 'Евгений'].each { |name| puts name }
   end
 
 end
 
 class Train # Класс Train (Поезд)
   #TYPE = [:passanger, :cargo]
-  #@@ind = 0
-
-  # Может принимать маршрут следования (объект класса Route).
-  def set_route(to_route)
-    @ind = 1
-    @to_route = to_route
-    # При назначении маршрута поезду, поезд автоматически помещается на первую станцию в маршруте.
-    @current_station = to_route.route[@ind]
-    @next_station = to_route.route[@ind + 1]
-    @past_station = to_route.route[@ind - 1]
-  end
-
-  attr_reader :to_route
-
-  #чтобы иметь перечисление неких значений в классе используют константы и массивы, например:
+  # чтобы иметь перечисление неких значений в классе используют константы и массивы, например:
   # class Train; TYPE = [:passanger, :cargo]; end
   # Константа - это любое значение, не обязательно массив; ее можно использовать и в других классах, обращаясь так: Train::TYPES.
+
   # Имеет номер (произвольная строка) и тип (грузовой, пассажирский) и количество вагонов, эти данные указываются при создании экземпляра класса
-  def initialize(nomer, tip, vagoni)
-    @nomer = nomer
-    #@tipg = []
-    #@tipp = []
-    @tip = tip # здесь к типу ещё нельзя обращаться
-    @vagoni = (1..vagoni).to_a
-    @speed
-    @current_station
-    @next_station
-    @past_station
-    #@train = [nomer, tip, vagoni]
+  def initialize(number, type, wagons) # initialize должен быть выше других методов
+      @number = number
+      @type = type # здесь к типу ещё нельзя обращаться
+      @wagons = (1..wagons).to_a
+      @speed = 0
+
+      #@current_station # без значение это не инициализация, а обращение; т.е. бесполезная строка
+      #@next_station
+      #@past_station
+
   end
 
-  attr_reader :tip # теперь можно обращаться к типу извне
+  # Может принимать маршрут следования (объект класса Route).
+  def set_route(route) #
+    #@index = 0
+    @route = route
+    # При назначении маршрута поезду, поезд автоматически помещается на первую станцию в маршруте.
+    @current_station_index = 0
+    #@current_station = route.stations[@current_station_index]
+    # @next_station = route.stations[@index + 1]
+    # @past_station = route.stations[@index - 1]
+  end
+
+  attr_reader :route, :type #, :current_station_index # index check
+
+  #attr_reader :type # теперь можно обращаться к типу извне
 
   # Может набирать скорость
   attr_writer :speed
@@ -126,30 +126,32 @@ class Train # Класс Train (Поезд)
   #end
 
   # Может тормозить (сбрасывать скорость до нуля)
-  def tormoz
+  def break
     self.speed = 0
     # self.speed(0) - не будет работать с геттером attr_reader :speed
     # speed(0)
   end
 
   # Может возвращать количество вагонов
-  def sostav
-    @vagoni
+  def wagons
+    @wagons.count
+    # @wagons
   end
 
   # Может прицеплять/отцеплять вагоны (по одному вагону за операцию, метод просто увеличивает или уменьшает количество вагонов). Прицепка/отцепка вагонов может осуществляться только если поезд не движется.
-  def sostav_add(vagoni)
+  def add_wagons(wagons)
     if @speed == 0
-      @vagoni.push(vagoni) #@vagoni << vagoni
+      @wagons.push(wagons) #@wagons << wagons
 
     else
       puts "Саш, ну поезд же едет, ну ты чего? ޏ₍ὸ.ό₎ރ"
     end
   end
 
-  def sostav_delete
+  def delete_wagons(wagons)
     if @speed == 0
-      @vagoni.delete_at(-1)
+      @wagons.delete_at(wagons)
+      #@wagons.delete_at(-1)
 
     else
       puts "Саш, ну поезд же едет, ну ты чего? ޏ₍ὸ.ό₎ރ"
@@ -157,28 +159,49 @@ class Train # Класс Train (Поезд)
   end
 
   # Может перемещаться между станциями, указанными в маршруте. Перемещение возможно вперед и назад, но только на 1 станцию за раз.
-  def go
-    @ind += 1
+  def forward
+    # тут нужна проверка на последнюю станцию, иначе поезд может уехать в пустоту
+    #if @current_station_index != -1
+    #@current_station = route.stations[@current_station_index]
+    route.stations[@current_station_index].dispatch(self) # это удаление поезда с текущей станции
+    #end
     #next_station = to_route[1]
-    @current_station = to_route.route[@ind]
+    @current_station_index += 1
+    #@index += 1
+    #@current_station = route.stations[@current_station_index]
+    route.stations[@current_station_index].arrival(self) # вызов объекта другого класса с его методом
     #current_station +=1
   end
 
   def back
-    @ind -= 1
-    @current_station = to_route.route[@ind]
+    if @current_station_index > 0
+      #@current_station.dispatch(self)
+      route.stations[@current_station_index].dispatch(self) #из-за того, что убрал инициализацию current_station, получилась вот такая жесть. Имхо лучше бы оставил
+      @current_station_index -= 1
+      #@current_station = route.stations[@current_station_index]
+      route.stations[@current_station_index].arrival(self)
+      #@current_station.arrival(self)
+    end
+    #@index -= 1
+    #@current_station_index -= 1
+    #@current_station = route.stations[@index]
+    #@current_station.arrival(self)
     #current_station -=1
   end
 
   # Возвращать предыдущую станцию, текущую, следующую, на основе маршрута / (past, current, next)
-  attr_reader :current_station, :next_station, :past_station
+  #attr_reader :current_station, :next_station, :past_station
+
+  def current_station
+    @current_station = route.stations[@current_station_index]
+  end
 
   def next_station
-    @next_station = to_route.route[@ind + 1]
+    @next_station = route.stations[@current_station_index + 1]
   end
 
   def past_station
-    @past_station = to_route.route[@ind - 1]
+    @past_station = route.stations[@current_station_index - 1]
   end
 
 =begin
@@ -210,19 +233,20 @@ class Train # Класс Train (Поезд)
 end
 
 =begin
-
 q = Station.new('q')
 w = Station.new('w')
 e = Station.new('e')
-a = Route.new(q.name, e.name)
-a.add_station(w.name)
-z = Train.new(1, 1, 1)
-x = Train.new(2, 2, 2)
-c = Train.new(3, 1, 1)
+a = Route.new(q, e)
+a.add_station(w)
+z = Train.new(1, :cargo, 1)
+x = Train.new(2, :cargo, 2)
+c = Train.new(3, :passenger, 1)
 q.arrival(z)
 q.arrival(x)
 q.arrival(c)
-z.set_route(a.to_route)
-q.spisok_tip
+z.set_route(a)
+x.set_route(a)
+z.speed=50
 
+nil
 =end
